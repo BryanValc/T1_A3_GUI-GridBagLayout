0c219d1e09c2d10153b59a8610bbd584d432b782
Bryan Valdez <bryan501th@gmail.com>
2021-02-12 15:22:37 -0600

    Se hicieron las distintas importaciones, extensiones e implementaciones necesarias

---
+++ src/PruebaGUIGridBagLayout.java
@@ -0,0 +1,39 @@
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+class GridBgLayout extends JFrame implements ActionListener{
+
+	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
+public class PruebaGUIGridBagLayout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
